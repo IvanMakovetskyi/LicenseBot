70b53c20624ccbc0b4d75fabd6f0f8699b0b4c0c
IvanMakovetskyi <im061056@gmail.com>
2026-04-09 13:22:26 -0700

    new message storage

---
+++ app/messages/stateMessageMap.py
@@ -0,0 +1,51 @@
+MESSAGE_MAP = {
+    "CA": [
+        "welcome",
+        "payment",
+        "form",
+        "documents_ready",
+        "license_approved",
+        "bord_created",
+        "application_completed",
+        "bord_message",
+        "email_check",
+        "congratulations",
+        "review",
+    ],
+
+    "FL": [
+        "welcome",
+        "payment",
+        "form",
+        "documents_ready",
+        "license_approved",
+        "bord_created",
+        "application_completed",
+        "email_check",
+        "congratulations",
+        "review",
+    ],
+
+    "NY": [
+        "welcome",
+        "payment",
+        "form",
+        "account_check",
+        "documents_ready",
+        "review",
+    ],
+
+    "PA": [
+        "welcome",
+        "payment",
+        "form",
+        "documents_ready",
+        "review",
+    ],
+
+    "ADDITIONAL_MESSAGES": [
+        "ready_for_payment",
+        "thanks_for_information",
+        "successfully_received"
+    ]
+}
