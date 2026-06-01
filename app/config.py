7c0994ab26dde2bc3b1f54a8513f8fc6d4d26927
Ivan Makovetskyi <im061056@gmail.com>
2026-06-01 15:31:46 -0700

    email change

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv # type: ignore
 
 load_dotenv()
 
 class Settings:
     TOKEN = os.getenv("TOKEN")
-    ADMINS = {838498434, 522072812, 804182735}
+    ADMINS = {int(admin) for admin in os.getenv("ADMINS").split(",") if admin}
     APP_ENV: str = os.getenv("APP_ENV", "prod")
 
 settings = Settings()
